79ae5dc0b68d07e1237cb6e166bcea3bc3f6f477
Bohyeon Kim <love.free.work@gmail.com>
2022-07-24 14:52:30 +0900

    change mongo to local

--- schemas/index.js
+++ schemas/index.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 const mongo_pw = "test:sparta";
+const mongo_atlas = `mongodb+srv://${mongo_pw}@cluster0.plrlvlp.mongodb.net/?retryWrites=true&w=majority`;
+const mongo_local = "mongodb://localhost:27017/node_api";
 //
 const connect = () => {
   mongoose
-    .connect(
-      `mongodb+srv://${mongo_pw}@cluster0.plrlvlp.mongodb.net/?retryWrites=true&w=majority`,
-      { ignoreUndefined: true }
-    )
+    .connect(mongo_local, { ignoreUndefined: true })
     .catch((err) => console.error(err));
 };
 
