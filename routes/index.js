cdb12e8de3bcba2aadfd250633b35423459ce251
Bohyeon Kim <love.free.work@gmail.com>
2022-07-26 15:51:25 +0900

    redirect Readme.md on '/api' path

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const marked = require("marked");
 
 // const Post = require("../schemas/post.js"); // post의 모델 스키마 불러옴
 // const Comment = require("../schemas/comment.js"); // comment의 DB 모델 스키마 불러옴
 // const Index = require("../schemas/index.js"); // post의 모델 스키마 불러옴
 
 // Route the 2 paths "/api/comments, /api/posts"
+
+marked.use({
+  pedantic: false,
+  gfm: true,
+  breaks: true,
+  smartLists: true,
+  smartypants: false,
+  xhtml: true,
+});
+
 const commentsRouter = require("./comments.js");
 const postsRouter = require("./posts.js");
+
+router.get("/", async (req, res) => {
+  var path = __dirname + "/../README.md";
+  var file = fs.readFileSync(path, "utf8");
+  res.send(marked.parse(file.toString()));
+});
 
 router.use("/posts", [postsRouter]);
 router.use("/comments", [commentsRouter]);
