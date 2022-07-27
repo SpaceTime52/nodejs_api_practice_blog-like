86d2850812603537c86e34c9a72ed58127795b27
Bohyeon Kim <love.free.work@gmail.com>
2022-07-27 22:10:30 +0900

    comments

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
+// express 모듈을 불러오고, 보안(CORS),포트 등 환경 초기화
 const express = require("express");
 const cors = require("cors");
 const port = 3000;
 
-// express 객체 선언, middleware 설치
+// express 객체 선언, 각종 middleware 설치
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// mongoDB Connection
+// mongoDB에 연결
 const connect = require("./schemas/index.js");
 connect();
 
-// route the "/api" path
-const indexRouter = require("./routes"); // /index.js 생략
+// "/api" path로 연결하는 라우터 연결 (우선 routes/index.js로)
+const indexRouter = require("./routes/index.js");
 app.use("/api", [indexRouter]);
 
-// Open the port, and keep Listening..
+// 포트 열어서 Request Listening..
 app.listen(port, () => {
   console.log(`${port} 번 포트로 연결이 완료되었습니다.`);
 });
--- schemas/comment.js
+++ schemas/comment.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose"); // 몽구스를 사용하겠다.
+const mongoose = require("mongoose"); // 몽구스를 사용하겠다는 선언
 
+// 몽구스로 comment라는 객체는 이런 모양으로 받겠다고 선언
 const commentSchema = new mongoose.Schema({
   _postId: {
-    type: String,
-    required: true,
+    type: String, // 문자열
+    required: true, // 필수로 입력해야 함~~
   },
   user: {
     type: String,
