4b82e5ae828b3ee581b0fb16d62efaec639a03d8
Bohyeon Kim <love.free.work@gmail.com>
2022-07-28 10:45:40 +0900

    satisfy all the assignments conditions

--- routes/comments.js
+++ routes/comments.js
@@ -5,10 +5,6 @@
 const express = require("express");
 const router = express.Router();
 
-// (ObjectId라는 속성의 객체로 자동 생성되는) 몽고DB의 '_id' 값을
-// 일반 문자열과 비교하고 서로 호환시키기 위한 외부 모듈이 필요합니다.
-const ObjectId = require("mongodb").ObjectID;
-
 // 이 파일에서 사용할 post와 comment DB가 어떻게 생겼는지 불러옵니다. (schema/comment.js)
 const Comment = require("../schemas/comment.js");
 const Post = require("../schemas/post.js");
@@ -16,7 +12,7 @@ const Post = require("../schemas/post.js");
 //  ---------------- 여기부터 API 시작 ----------------
 
 // ------------------
-// 댓글 작성 with POST ('/api/comments/_postId')
+// TASK 1. 댓글 작성 with POST ('/api/comments/_postId')
 router.post("/:_postId", async (req, res) => {
   // URL 뒤쪽에 params로 전달받은 _postId를 사용하겠다고 변수 선언합니다.
   const { _postId } = req.params;
@@ -24,8 +20,13 @@ router.post("/:_postId", async (req, res) => {
   // request body 에 적힌 변수들을 기록해둡니다.
   const { user, password, content } = req.body;
 
+  // body에 입력받은 수정할 댓글이 없으면 수정할 수 없습니다~~
+  if (!content) {
+    return res.json({ message: "댓글 내용을 입력해주세요" });
+  }
+
   // _postId 와 일치하는 데이터를 DB에서 찾습니다.
-  const posts = await Post.find({ _id: ObjectId(_postId) });
+  const posts = await Post.find({ _id: _postId });
 
   // 찾은 게 없으면, 종료합니다.
   if (!posts.length) {
@@ -47,13 +48,13 @@ router.post("/:_postId", async (req, res) => {
 });
 
 // ------------------
-// 댓글 목록 조회 with GET ('/api/comments/_postId')
+// TASK 2. 댓글 목록 조회 with GET ('/api/comments/_postId')
 router.get("/:_postId", async (req, res) => {
   // URL 뒤쪽에 params로 전달받은 _postId를 사용하겠다고 변수 선언합니다.
   const { _postId } = req.params;
 
-  // postId가 일치하는 게시글을 찾아보고,
-  const posts = await Post.find({ _id: ObjectId(_postId) });
+  // postId가 일치하는 게시글을 되도록 날짜 내림차순으로 불러와 찾아보고,
+  const posts = await Post.find({ _id: _postId }).sort({ createdAt: -1 });
 
   // 없으면 댓글을 못씁니다~~
   if (!posts.length) {
@@ -64,7 +65,7 @@ router.get("/:_postId", async (req, res) => {
   const allCommentInfo = await Comment.find({ _postId });
   const data = [];
 
-  // 이 게시물의 댓글을 하나씩 돌면서, 배열에 넣어서 반환합니다.
+  // 이 게시물의 댓글을 하나씩 돌면서, 응답할 배열에 넣어서 반환합니다.
   for (let i = 0; i < allCommentInfo.length; i++) {
     data.push({
       commentId: allCommentInfo[i]._id.toString(),
@@ -74,8 +75,82 @@ router.get("/:_postId", async (req, res) => {
     });
   }
 
-  // 반환값은 Response json으로 전달
+  // 완성된 배열은 명세서와 동일한 모양으로 나오도록 가공하여, Response json으로 응답함
   res.json({ data: data });
+});
+
+// ------------------
+// TASK 3. 댓글 수정 with PUT ('/api/comments/_commentId')
+router.put("/:_commentId", async (req, res) => {
+  // params로 전달 받은 댓글번호 _commentId와
+  const { _commentId } = req.params;
+  // body로 전달 받은 password와 content를 확보합니다.
+  const { password, content } = req.body;
+
+  // params 로 입력받은 _commentId에 해당하는 댓글을 찾아서,
+  const comments = await Comment.find({ _id: _commentId });
+  // 해당하는 댓글이 없으면 수정할 수 없습니다~~
+  if (!comments.length) {
+    return res.json({ message: "해당 댓글이 없습니다." });
+  }
+
+  // body에 입력받은 수정할 댓글이 없으면 수정할 수 없습니다~~
+  if (!content) {
+    return res.json({ message: "댓글 내용을 입력해주세요" });
+  }
+
+  // 찾은 게 있으면 그 password와 body에 입력한 password를 비교합니다.
+  const db_password = comments["password"];
+  if (db_password != password) {
+    return res.json({ message: "비밀번호를 확인해주세요." });
+  }
+
+  // 해당 댓글을 업데이트 합니다.
+  await Comment.updateOne(
+    {
+      _id: _commentId, // 어떤 댓글을 수정할지 넣고,
+    },
+    {
+      // 뭐라고 수정할지 정의합니다.
+      $set: {
+        password: password,
+        content: content,
+      },
+    }
+  );
+
+  // 수정이 끝났으므로 메세지를 Response 합니다.
+  res.json({ message: "댓글을 수정하였습니다." });
+});
+
+// ------------------
+// TASK 4.게시글 삭제 with DELETE ('/api/comments/_commentId')
+router.delete("/:_commentId", async (req, res) => {
+  // params로 전달 받은 댓글번호 _commentId와
+  const { _commentId } = req.params;
+  // 바디를 통해 password 를 받습니다.
+  const { password } = req.body;
+
+  // _commentId 와 일치하는 comments를 불러옵니다.
+  const comments = await Comment.findOne({ _id: _commentId });
+
+  // 찾은 게 없으면 삭제할 수 없습니다.
+  if (!comments) {
+    return res.json({ message: "해당 댓글이 없습니다." });
+  }
+
+  // 찾은 게 있으면 그 password와 body에 입력한 password를 비교합니다.
+  const db_password = comments["password"];
+  if (db_password != password) {
+    return res.json({ message: "비밀번호를 확인해주세요." });
+
+    // password 일치하면 그걸 지웁니다~~
+  } else {
+    await Comment.deleteOne({ _id: _commentId });
+
+    // 지웠으니까 여기서 메세지를 Response 합니다.
+    return res.json({ message: "댓글을 삭제하였습니다." });
+  }
 });
 
 // ------------------
@@ -84,7 +159,7 @@ router.get("/:_postId", async (req, res) => {
 router.post("/:_postId/many", async (req, res) => {
   const { _postId } = req.params;
 
-  const posts = await Post.find({ _id: ObjectId(_postId) });
+  const posts = await Post.find({ _id: _postId });
   if (!posts.length) {
     return res.json({ message: "해당 게시글이 없습니다." });
   }
@@ -104,70 +179,4 @@ router.post("/:_postId/many", async (req, res) => {
   res.json({ message: "댓글을 생성하였습니다." });
 });
 
-// 댓글 수정 with PUT ('/api/comments/_commentId')
-router.put("/:_commentId", async (req, res) => {
-  // params로 전달 받은 댓글번호 _commentId와
-  const { _commentId } = req.params;
-  // body로 전달 받은 password와 content를 확보합니다.
-  const { password, content } = req.body;
-
-  // 해당 댓글을 찾아서,
-  const comments = await Comment.find({ _id: ObjectId(_commentId) });
-
-  // 댓글이 없으면 수정할 수 없습니다~~
-  if (!comments.length) {
-    return res.json({ message: "해당 댓글이 없습니다." });
-  }
-
-  // 해당 댓글을 업데이트 합니다.
-  await Comment.updateOne(
-    {
-      _id: ObjectId(_commentId), // 어떤 댓글을 수정할지 넣고,
-    },
-    {
-      // 뭐라고 수정할지 정의합니다.
-      $set: {
-        password: password,
-        content: content,
-      },
-    }
-  );
-
-  // 수정이 끝났으므로 메세지를 Response 합니다.
-  res.json({ message: "댓글을 수정하였습니다." });
-});
-
-// 게시글 삭제 with DELETE ('/api/comments/_commentId')
-router.delete("/:_commentId", async (req, res) => {
-  // params로 전달 받은 댓글번호 _commentId와
-  const { _commentId } = req.params;
-  // 바디를 통해 password 를 받습니다.
-  const { password } = req.body;
-
-  // _commentId 와 일치하는 comments를 불러옵니다.
-  const comments = await Comment.find({ _id: ObjectId(_commentId) });
-
-  // 찾은 게 없으면 삭제할 수 없습니다.
-  if (!comments.length) {
-    return res.json({ message: "해당 댓글이 없습니다." });
-  }
-
-  // 찾은 게 있으면 그 password와 body에 입력한 password를 비교합니다.
-  const db_password = comments[0]["password"];
-  if (db_password != password) {
-    return res.json({ message: "비밀번호를 확인해주세요." });
-
-    // password 일치하면 그걸 지웁니다~~
-  } else {
-    await Comment.deleteOne({ _id: ObjectId(_commentId) });
-
-    // 지웠으니까 여기서 메세지를 Response 합니다.
-    return res.json({ message: "댓글을 삭제하였습니다." });
-  }
-});
-
 module.exports = router;
-
-// if (!posts.length) {
-//   return res.json({ message: "해당 게시글이 없습니다." });
-// }
--- routes/posts.js
+++ routes/posts.js
@@ -5,20 +5,16 @@
 const express = require("express");
 const router = express.Router();
 
-// (ObjectId라는 속성의 객체로 자동 생성되는) 몽고DB의 '_id' 값을
-// 일반 문자열과 비교하고 서로 호환시키기 위한 외부 모듈이 필요합니다.
-const ObjectId = require("mongodb").ObjectID;
-
 // 이 파일에서 사용할 post DB가 어떻게 생겼는지 불러옵니다. (schema/post.js)
 const Post = require("../schemas/post.js");
 
 //  ---------------- 여기부터 API 시작 ----------------
 
 // ------------------
-// 게시글 조회 with GET ('/api/posts')
+// TASK 1 : 게시글 조회 with GET ('/api/posts')
 router.get("/", async (req, res) => {
-  // 몽고디비 데이터베이스 상의 'Post'에서 모든 데이터를 불러온 후,
-  const dataAll = await Post.find();
+  // 몽고디비 데이터베이스 상의 'Post'에서 모든 데이터를 createdAt의 내림차순으로 불러온 후,
+  const dataAll = await Post.find().sort({ createdAt: -1 });
 
   // data 배열에 하나씩 넣어 줍니다. (push)
   const data = [];
@@ -35,7 +31,7 @@ router.get("/", async (req, res) => {
 });
 
 // ------------------
-// 게시글 작성 with POST ('/api/posts')
+// TASK 2 : 게시글 작성 with POST ('/api/posts')
 router.post("/", async (req, res) => {
   // POST 요청의 body로 받은 아이들을 각 변수 user, password, title, content에 넣어줍니다.
   const { user, password, title, content } = req.body;
@@ -54,49 +50,27 @@ router.post("/", async (req, res) => {
 });
 
 // ------------------
-// // 게시글 작성 여러개 한꺼번에 with POST ('/api/posts/many')
-// // 명세서에 없는 내용이지만 한번에 여러개 게시글을 작성해놓기 위해서 만들어본 것입니다. 건너 뛰셔도 됩니다.
-router.post("/many", async (req, res) => {
-  for (let i = 0; i < req.body.length; i++) {
-    var { user, password, title, content } = req.body[i];
-
-    await Post.create({
-      user,
-      password,
-      title,
-      content,
-      createdAt: new Date(),
-    });
-  }
-
-  res.json({ message: "게시글을 생성하였습니다." });
-});
-
-// ------------------
-// 게시글 상세조회 with GET ('/api/posts/:_postId')
+// TASK 3 : 게시글 상세조회 with GET ('/api/posts/:_postId')
 router.get("/:_postId", async (req, res) => {
   // URL 뒤쪽에 params{ 로 전달받은 _postId를 사용하겠다고 변수 선언합니다.
   const { _postId } = req.params;
 
   // 이 _postId를 id로 가진 DB 요소를 모두 찾아서 thisPost라는 변수에 넣습니다.
-  const thisPost = await Post.find({ _id: ObjectId(_postId) });
+  const thisPost = await Post.findOne({ _id: _postId });
 
   // DB에서 찾아낸 thisPost의 개수가 0개이면, 없다고 response 합니다.
-  if (!thisPost.length) {
+  if (!thisPost) {
     return res.json({ message: "해당 게시글이 없습니다." });
   }
 
   // 그렇지 않으면,
-  // 찾아낸 모든 post를 하나씩 돌면서. 그 id값이 _postId와 같은 걸 찾아 반환할 data에 넣어줍니다.
-  const posts = await Post.find();
-  const filteredPosts = posts.filter((e) => e["_id"].toString() === _postId);
   const data = [
     {
-      postId: filteredPosts[0]._id.toString(),
-      user: filteredPosts[0].user,
-      title: filteredPosts[0].title,
-      content: filteredPosts[0].content,
-      createdAt: filteredPosts[0].createdAt,
+      postId: thisPost._id.toString(),
+      user: thisPost.user,
+      title: thisPost.title,
+      content: thisPost.content,
+      createdAt: thisPost.createdAt,
     },
   ];
 
@@ -105,7 +79,7 @@ router.get("/:_postId", async (req, res) => {
 });
 
 // ------------------
-// 게시글 수정 with PUT ('/api/posts/:_postId')
+// TASK 4 : 게시글 수정 with PUT ('/api/posts/:_postId')
 router.put("/:_postId", async (req, res) => {
   // URL 뒤쪽에 params로 전달받은 _postId를 사용하겠다고 변수 선언합니다.
   const { _postId } = req.params;
@@ -113,17 +87,24 @@ router.put("/:_postId", async (req, res) => {
   const { password, title, content } = req.body;
 
   // 이 _postId를 id로 가진 DB 요소를 모두 찾아서 thisPost라는 변수에 넣습니다.
-  const thisPost = await Post.find({ _id: ObjectId(_postId) });
+  const thisPost = await Post.findOne({ _id: _postId });
 
   // 마찬가지로 찾아낸 게 없으면 게시글 수정을 진행할 수 없습니다.
-  if (!thisPost.length) {
+  if (!thisPost) {
     return res.json({ message: "해당 게시글이 없습니다." });
   }
 
-  // 찾아낸 게 있으면, 그 Post를 update합니다.
+  // 찾아낸 게 있으면, 찾아둔 thisPost의 password와 입력받은 패스워드를 비교해보고,
+  const db_password = thisPost["password"];
+  if (password != db_password) {
+    console.log(password, db_password);
+    return res.json({ message: "패스워드가 일치하지 않습니다." });
+  }
+
+  // 비밀번호까지 일치하면 if문을 거치지 않고 여기까지 오는데, 그 Post를 update합니다.
   await Post.updateOne(
     {
-      _id: ObjectId(_postId),
+      _id: _postId,
       // updateOne 메소드는 첫번째 인자로 업데이트할 DB의 고유값을 받습니다. (여기선 _id)
     },
     {
@@ -141,25 +122,22 @@ router.put("/:_postId", async (req, res) => {
 });
 
 // ------------------
-// 게시글 삭제 with DELETE ('/api/posts/:_postId')
+// TASK 5 : 게시글 삭제 with DELETE ('/api/posts/:_postId')
 router.delete("/:_postId", async (req, res) => {
   // URL 뒤쪽에 params로 전달받은 _postId를 사용하겠다고 변수 선언합니다.
   const { _postId } = req.params;
-
   // 비교를 위해서 body에 담아 받은 password를 password변수에 그대로 담습니다.
   const { password } = req.body;
 
   // 입력 받은 _postId와 동일한 요소를 DB에서 찾아냅니다.
-  const thisPost = await Post.find({ _id: ObjectId(_postId) });
+  const thisPost = await Post.findOne({ _id: _postId });
   // 찾은 게 없으면 실패를 Response 하고,
-  if (!thisPost.length) {
+  if (!thisPost) {
     return res.json({ message: "해당 게시글이 없습니다." });
   }
 
-  // DB의 모든 데이터를 받고, 그걸 돌면서 '_postId'와 id 값이 같은 Post와 그 password를 찾아내고,
-  const posts = await Post.find();
-  const filteredPosts = posts.filter((e) => e["_id"].toString() === _postId);
-  const db_password = filteredPosts[0]["password"];
+  // 아까 찾은 thisPost의 password와
+  const db_password = thisPost["password"];
 
   // body로 입력받았던 password와 DB에서 찾아낸 password가 일치하지 않으면 요청 종료.
   if (password != db_password) {
@@ -168,12 +146,30 @@ router.delete("/:_postId", async (req, res) => {
     // 아니면 내용 삭제합니다.
   } else {
     await Post.deleteOne({
-      _id: ObjectId(_postId),
+      _id: _postId,
     });
-
     // 여기까지 왔으면 게시글이 삭제되었으므로 삭제하게 됩니다.
     res.json({ message: "게시글을 삭제하였습니다." });
   }
+});
+
+// ----부가기능--------------
+// // 게시글 작성 여러개 한꺼번에 with POST ('/api/posts/many')
+// // 명세서에 없는 내용이지만 한번에 여러개 게시글을 작성해놓기 위해서 만들어본 것입니다. 건너 뛰셔도 됩니다.
+router.post("/many", async (req, res) => {
+  for (let i = 0; i < req.body.length; i++) {
+    var { user, password, title, content } = req.body[i];
+
+    await Post.create({
+      user,
+      password,
+      title,
+      content,
+      createdAt: new Date(),
+    });
+  }
+
+  res.json({ message: "게시글을 생성하였습니다." });
 });
 
 // 이 파일의 router 객체를 외부에 공개합니다.
