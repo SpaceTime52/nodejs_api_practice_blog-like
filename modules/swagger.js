060670605c1fe1b85999e7db414b633a2d50c224
Bohyeon Kim <love.free.work@gmail.com>
2022-08-02 21:43:51 +0900

    edit swagger main html

--- modules/swagger.js
+++ modules/swagger.js
@@ -6,8 +6,13 @@ const options = {
     info: {
       title: "Blog API",
       version: "1.0.0",
-      description:
-        "API 연습을 위한 블로그 콘텐츠, 댓글 API - 명세는 아래와 같습니다.",
+      description: `</br>
+        (연습문제이므로 ERD도 여기에 기록합니다.)
+        <img src="https://user-images.githubusercontent.com/49124248/181908641-6efe0ad2-049a-4aae-9a4b-31a225662482.png" alt="image" width="600">
+        </br>
+        <b>API 연습을 위한 블로그 콘텐츠, 댓글 API</b> - 명세는 아래와 같습니다.
+        </br>
+        `,
     },
     host: "http://nodeapi.myspaceti.me:8000",
     basePath: "/",
